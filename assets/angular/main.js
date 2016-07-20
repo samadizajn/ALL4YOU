924aaca216b3e9558a048d8879afe3773b9636bb
sale14 <sale14sale@gmail.com>
2016-07-20 13:26:49 +0200

    sasa

--- assets/angular/main.js
+++ assets/angular/main.js
@@ -1,6 +1,6 @@
-var app = angular.module('plunker', []);
+var app = angular.module('all4you', []);
 
-app.controller('MainCtrl', function($scope, $window, $timeout) {
+app.controller('ScreenSize', function($scope, $window, $timeout) {
   $scope.windowWidth = $window.innerWidth;
   
   $window.onresize = function(event) {
--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,16 @@
 jQuery( document ).ready(function( $ ) {
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
+var $window = $(window),
+$html = $('.header-navigation ul.language_switcher');
 
+$window.resize(function resize() {
+	if ($window.width() < 514) {
+		return $html.addClass('mobile');
+	}
+
+  $html.removeClass('mobile');
+}).trigger('resize');
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 });
